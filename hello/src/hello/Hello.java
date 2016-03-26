3f41f636bfca10bf4bca6bf24cf48ac169d24ab2
fabien <fabien.kunis@gmail.com>
2016-03-26 13:42:57 +0200

    Array shuffle algorithms.

--- hello/src/hello/Hello.java
+++ hello/src/hello/Hello.java
@@ -10,13 +10,39 @@ public class Hello {
 //		System.out.println("Total: " + (x + y));
 //		System.out.println("Total: " + x + y);
 //		System.out.println(x + y + " Total");
-		try {
-			String name = args[0];
-			System.out.println("Hello, " + name);
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("Hello nobody");
+//		try {
+//			String name = args[0];
+//			System.out.println("Hello, " + name);
+//		} catch (ArrayIndexOutOfBoundsException e) {
+//			System.out.println("Hello nobody");
+//		}
+		int[] deck = new int[52];
+		for(int i = 0; i < deck.length; i++){
+			deck[i] = i;
 		}
+		printArray(deck);
+		System.out.println();
+		int N = deck.length; 
+		for (int i = 0; i < N; i++) 
+		{ 
+		   int r = i + (int) (Math.random() * (N-i)); 
+		   System.out.println("r=" + r);
+		   int t = deck[r]; 
+		   //System.out.println("t=" + t);
+		   deck[r] = deck[i]; 
+		   //System.out.println("deck[r]=" + deck[r]);
+		   deck[i] = t; 
+		   //System.out.println("deck[i]=" + deck[i]);
+		} 
+		printArray(deck);
 
+	}
+
+	private static void printArray(int[] deck) {
+		for (int i = 0; i < deck.length; i++) {
+			System.out.print(deck[i] + ",");
+		}
+		
 	}
 
 }
